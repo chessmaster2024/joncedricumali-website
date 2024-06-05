fa41af38ef1719b8b50902f4f05fd44057efec9e
chessmaster2024 <joncedricu@gmail.com>
2024-06-05 20:24:39 +0800

    Add files via upload

---
+++ My Projects/Question and answer/JS/js.js
@@ -0,0 +1,70 @@
+function checkAnswer1(answer) {
+    if (answer === 'A') {
+      document.getElementById('result').innerHTML += '<p class="correct">Correct! </p>';
+    } else {
+      document.getElementById('result').innerHTML += '<p class="wrong">Wrong! </p>';
+    }
+}
+function checkAnswer2(answer) {
+    if (answer === 'A') {
+      document.getElementById('result').innerHTML += '<p class="correct">Correct! </p>';
+    } else {
+      document.getElementById('result').innerHTML += '<p class="wrong">Wrong! </p>';
+    }
+}
+function checkAnswer(answer) {
+    if (answer === 'B') {
+      document.getElementById('result').innerHTML += '<p class="correct">Correct! </p>';
+    } else {
+      document.getElementById('result').innerHTML += '<p class="wrong">Wrong!</p>';
+    }
+}
+function checkAnswer(answer) {
+    if (answer === 'C') {
+      document.getElementById('result').innerHTML += '<p class="correct">Correct! </p>';
+    } else {
+      document.getElementById('result').innerHTML += '<p class="wrong">Wrong!</p>';
+    }
+}
+function checkAnswer5(answer) {
+    if (answer === 'C') {
+      document.getElementById('result').innerHTML += '<p class="correct">Correct! </p>';
+    } else {
+      document.getElementById('result').innerHTML += '<p class="wrong">Wrong! </p>';
+    }
+}
+function checkAnswer(answer) {
+    if (answer === 'A') {
+      document.getElementById('result').innerHTML += '<p class="correct">Correct! </p>';
+    } else {
+      document.getElementById('result').innerHTML += '<p class="wrong">Wrong!</p>';
+    }
+}
+function checkAnswer(answer) {
+    if (answer === 'B') {
+      document.getElementById('result').innerHTML += '<p class="correct">Correct! </p>';
+    } else {
+      document.getElementById('result').innerHTML += '<p class="wrong">Wrong! </p>';
+    }
+}
+function checkAnswer(answer) {
+    if (answer === 'A') {
+      document.getElementById('result').innerHTML += '<p class="correct">Correct!</p>';
+    } else {
+      document.getElementById('result').innerHTML += '<p class="wrong">Wrong! </p>';
+    }
+}
+function checkAnswer(answer) {
+    if (answer === 'C') {
+      document.getElementById('result').innerHTML += '<p class="correct">Correct! </p>';
+    } else {
+      document.getElementById('result').innerHTML += '<p class="wrong">Wrong!</p>';
+    }
+}
+function checkAnswer(answer) {
+    if (answer === 'B') {
+      document.getElementById('result').innerHTML += '<p class="correct">Correct!</p>';
+    } else {
+      document.getElementById('result').innerHTML += '<p class="wrong">Wrong!</p>';
+    }
+}
